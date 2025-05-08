e07a3608abfd4649410e9db5586b80f02692bc85
oscarfeeler <oscarbo56@gmail.com>
2025-05-08 14:03:04 +0200

    se ha añadido el script client.py y se ha añadido funcionalidad al script server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import socket
 
 app = Flask(__name__)
@@ -19,9 +19,15 @@ def get_ipv4_address():
 # Constants for client
 LOCAl_IP = get_ipv4_address()
 DEFAULT_PORT = 5000
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def recibir_mensajes():
+    mensaje_recibido = request.json
+    if mensaje_recibido:
+        print(f"Mensaje recibido: {mensaje_recibido}")
+        return "Mensaje recibido"
+    else:
+        return "No se recibió ningún mensaje"
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=DEFAULT_PORT, debug=False)
